1713052d9d0672e14617b53eab855a646d8a9a60
jamieherrera24 <jhs24@gcloud.ua.es>
2023-10-31 16:22:46 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: "/portafolio/",
+
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+});
